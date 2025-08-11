ae5e251488a8d02eb35221af02f43f9571767d6d
Nick Harder <nick.harder94@gmail.com>
2025-08-11 16:13:18 +0200

    Change name from default to ENTSOE-E Analytics Bot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Agent Chat",
-  description: "Agent Chat UX by LangChain",
+  title: "ENTSOE-E Analytics Bot",
+  description: "ENTSOE-E Analytics Bot UX by Nick",
 };
 
 export default function RootLayout({
