b18eed96e7d555f7e50e8e1c2a0c6e62883b479c
Nick Harder <nick.harder94@gmail.com>
2025-08-14 12:33:44 +0200

    Remove plot UI element

--- src/components/custom/index.tsx
+++ src/components/custom/index.tsx
@@ -1,10 +1,8 @@
-import PlotHTML, { PlotHTMLProps } from "./plot";
 import ToolLoading, { ToolLoadingProps } from "./loading-spinner";
 import ToolDone from "./tool-done";
 
 // Define component prop types
 export type ComponentProps = {
-  plot: PlotHTMLProps;
   toolLoading: ToolLoadingProps;
   toolDone: { message: string };
   // Add other component types here as you create them
@@ -12,7 +10,6 @@ export type ComponentProps = {
 
 // Type-safe component map
 const ComponentMap = {
-  plot: PlotHTML,
   toolLoading: ToolLoading,
   toolDone: ToolDone,
 } as const satisfies Record<keyof ComponentProps, React.ComponentType<any>>;
